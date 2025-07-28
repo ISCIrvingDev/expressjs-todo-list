f6a1442dff227b9d14c7cba27f3f433a453fe71a
Irving Salazar <ISCIrvingDev@gmail.com>
2025-07-28 13:15:05 -0600

    Quitar el servidor estatico

--- app.js
+++ app.js
@@ -3,24 +3,26 @@ const express = require('express'),
     logger = require('morgan'),
 
     mongoose = require('mongoose'),
-    db = require(`${__dirname}/db/conn`) (mongoose),
-    models = require(`${__dirname}/models`) (mongoose),
-    routes = require(`${__dirname}/routes`) (models),
+    db = require(`${__dirname}/db/conn`)(mongoose),
+    models = require(`${__dirname}/models`)(mongoose),
+    routes = require(`${__dirname}/routes`)(models),
     app = express()
     ;
 
 app
     .set('port', (process.env.PORT || 3000))
 
-    .use(express.static(`${__dirname}/public`))
+    // Comentar servidor estatico
+    // .use(express.static(`${__dirname}/public`))
+
     .use(express.json())
-    .use(express.urlencoded({extended: false}))
+    .use(express.urlencoded({ extended: false }))
 
     .use(logger('dev'))
 
-        // Solamente para hacer pruebas con CORS en development
+    // CORS
     .use((req, res, next) => {
-        // res.header('Access-Control-Allow-Origin', '*');
+        // res.header('Access-Control-Allow-Origin', '*'); // Solamente para hacer pruebas con CORS en development
         res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -32,7 +34,7 @@ app
     .use((err, req, res, next) => {
         // console.log('El error ocurrido es: ', err);
         console.log('Ruta no encontrada');
-        
+
         res.status(err.status || 500);
         return res.redirect('/');
         // res.status(err.status || 500);
